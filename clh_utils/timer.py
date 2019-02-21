618eff3f57b11c6913916db214bccdbe81dd78ba
linghao <3150105275@zju.edu.cn>
2019-02-21 19:53:45 +0800

    implement timer by Stack. add tests module.

--- clh_utils/timer.py
+++ clh_utils/timer.py
@@ -1,51 +1,39 @@
 import time
 
 
-# todo: use stack.
 class Timer:
-    def __init__(self, print_all=False, unit='ms'):
+    def __init__(self, unit='ms'):
         valid_units = ['ms', 's']
         _unit_functions = {'s': lambda x: x, 'ms': lambda x: x * 1000}
         if unit not in valid_units:
             raise Exception('expect unit in {}, but found {}'.format(valid_units, unit))
         self.unit = unit
         self.unit_function = _unit_functions[unit]
-        self._begin = 0
-        self._msg = ''
-        self.first_time = 0
+        self._msgs = []
+        self.begins = {}
         self.totals = {}
         self.times = {}
-        self.print_all = print_all
 
     def tic(self, msg=''):
-        self._msg = msg
+        self._msgs.append(msg)
         if msg not in self.totals:
             self.times[msg] = 0
             self.totals[msg] = 0
-        if self.print_all:
-            print(self._msg, 'begins')
-        self._begin = time.time()
-        if self.first_time == 0:
-            self.first_time = self._begin
+        self.begins[msg] = time.time()
 
     def toc(self):
         end = time.time()
-        if self.print_all:
-            print(self._msg, 'ends', end - self._begin)
-        self.totals[self._msg] += end - self._begin
-        self.times[self._msg] += 1
-        return end - self._begin
+        last_msg = self._msgs.pop()
+        self.totals[last_msg] += end - self.begins[last_msg]
+        self.times[last_msg] += 1
+        return end - self.begins[last_msg]
 
     def summarize(self):
 
-        total_avg = 0
         for k, v in self.totals.items():
-            try:
-                print('{} total {} times takes {:.2f} {}, average {:.2f} {}.'.format(
-                    k, self.times[k], self.unit_function(v), self.unit, self.unit_function(v / self.times[k]),
-                    self.unit))
-                total_avg += v / self.times[k]
+            print('{} total {} times takes {:.2f} {}, average {:.2f} {}.'.format(
+                k, self.times[k], self.unit_function(v), self.unit, self.unit_function(v / self.times[k]),
+                self.unit))
 
-            except ZeroDivisionError as e:
-                print(k)
-        print('total average {:.2f} {}, {:.2f}fps'.format(self.unit_function(total_avg), self.unit, 1 / total_avg))
+
+timer = Timer()
