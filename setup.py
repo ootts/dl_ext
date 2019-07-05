a3325e5a851dd0ce1f8b7cbd9228a42120d19df7
f-sky <939949352@qq.com>
2019-07-05 10:08:32 +0800

    0.7.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if sys.version_info < (3,):
 
 setup(
     name='dl_ext',
-    version='0.7.0',
+    version='0.7.2',
     description='Chen Linghao\'s personal utils.',
     packages=find_packages(),
     install_requires=[
