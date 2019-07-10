fe1ae6d45050e49b0336f3cd9699d8ea8cbce51b
f-sky <939949352@qq.com>
2019-07-10 11:00:01 +0800

    focal loss

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if sys.version_info < (3,):
 
 setup(
     name='dl_ext',
-    version='0.7.5',
+    version='0.7.6',
     description='Chen Linghao\'s personal utils.',
     packages=find_packages(),
     install_requires=[
